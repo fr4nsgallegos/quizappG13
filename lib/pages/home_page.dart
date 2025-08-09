44f39469fd9170d596373dd68eb9604ddfdfb653
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-08-09 11:11:49 -0500

    intertface

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,6 +5,56 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Color(0xff2F3243),
+      appBar: AppBar(
+        elevation: 15,
+
+        backgroundColor: Color(0xff2F3243),
+        title: Text("QuizzApp"),
+        centerTitle: true,
+        foregroundColor: Colors.white,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              flex: 5,
+              child: Center(
+                child: Text(
+                  "¿Has almorzado hoy?",
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: MaterialButton(
+                  color: Colors.greenAccent,
+                  minWidth: double.infinity,
+                  onPressed: () {},
+                  child: Text("Verdadero"),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: MaterialButton(
+                  color: Colors.redAccent,
+                  minWidth: double.infinity,
+                  onPressed: () {},
+                  child: Text("Falso"),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
