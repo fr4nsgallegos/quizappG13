495c88335b84dfd23a400e892247aff3823c257d
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-08-09 12:10:53 -0500

    idea de solucion para la tarea

--- lib/models/question_model.dart
+++ lib/models/question_model.dart
@@ -3,3 +3,17 @@ class QuestionModel {
   bool answer;
   QuestionModel({required this.question, required this.answer});
 }
+
+// class QuestionModel {
+//   String heroName;
+//   String urlImage;
+//   List<String> questionAlternatives;
+//   int indexCorrectAnswer;
+// }
+
+// QuestionModel question1 = QuestionModel("Capitán américa", [
+//   "Pedro",
+//   "Ironamain",
+//   "Maraviall",
+//   "Capitán américa",
+// ], 3);
